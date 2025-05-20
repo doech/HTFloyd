7bc5bb616a0e0bb4fab2f0202e0af72acb866fc8
ale sierra <malesierra15@gmail.com>
2025-05-20 15:27:55 -0600

    logica para algoritmo floyd

--- Grafo.java
+++ Grafo.java
@@ -1,5 +1,5 @@
 public class Grafo {
-    private final int;
+    private final int INF = 99999; // infinito para representar la ausencia de conexión
     private int[][] distancias;
     private int[][] camino;
     private String[] nombresNodos;
@@ -10,4 +10,85 @@ public class Grafo {
         distancias = new int[numNodos][numNodos];
         camino = new int[numNodos][numNodos];
         nombresNodos = new String[numNodos];
+
+        for (int i = 0; i < numNodos; i++) {
+            for (int j = 0; j < numNodos; j++) {
+                if (i == j) distancias[i][j] = 0;
+                else distancias[i][j] = INF;
+                camino[i][j] = -1;
+            }
+        }
     }
+
+    public void setNombreNodo(int indice, String nombre) {
+        nombresNodos[indice] = nombre;
+    }
+
+    public void agregarArco(int origen, int destino, int peso) {
+        distancias[origen][destino] = peso;
+        camino[origen][destino] = destino;
+    }
+
+    public void eliminarArco(int origen, int destino) {
+        distancias[origen][destino] = INF;
+        camino[origen][destino] = -1;
+    }
+
+    public void floydWarshall() {
+        for (int k = 0; k < numNodos; k++) {
+            for (int i = 0; i < numNodos; i++) {
+                for (int j = 0; j < numNodos; j++) {
+                    if (distancias[i][k] + distancias[k][j] < distancias[i][j]) {
+                        distancias[i][j] = distancias[i][k] + distancias[k][j];
+                        camino[i][j] = camino[i][k];
+                    }
+                }
+            }
+        }
+    }
+
+    public void imprimirRuta(int origen, int destino) {
+        if (camino[origen][destino] == -1) {
+            System.out.println("No hay ruta");
+            return;
+        }
+        System.out.print(nombresNodos[origen]);
+        while (origen != destino) {
+            origen = camino[origen][destino];
+            System.out.print(" -> " + nombresNodos[origen]);
+        }
+        System.out.println();
+    }
+
+    public void imprimirMatrizDistancias() {
+        System.out.println("\nMatriz de distancias:");
+        for (int i = 0; i < numNodos; i++) {
+            for (int j = 0; j < numNodos; j++) {
+                if (distancias[i][j] == INF) System.out.print("INF ");
+                else System.out.print(distancias[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public String obtenerCentroGrafo() {
+        int[] excentricidades = new int[numNodos];
+        for (int i = 0; i < numNodos; i++) {
+            int max = 0;
+            for (int j = 0; j < numNodos; j++) {
+                if (distancias[i][j] != INF && distancias[i][j] > max) {
+                    max = distancias[i][j];
+                }
+            }
+            excentricidades[i] = max;
+        }
+
+        int centro = 0;
+        for (int i = 1; i < numNodos; i++) {
+            if (excentricidades[i] < excentricidades[centro]) {
+                centro = i;
+            }
+        }
+        return nombresNodos[centro];
+    }
+}
--- Main.java
+++ Main.java
@@ -1,1 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        Grafo grafo = new Grafo(5);
 
+        // nombres de ciudades
+        grafo.setNombreNodo(0, "Ciudad de Guatemala"); // A
+        grafo.setNombreNodo(1, "Zacapa");              // B
+        grafo.setNombreNodo(2, "Chiquimula");          // C
+        grafo.setNombreNodo(3, "Quetzaltenango");      // D
+        grafo.setNombreNodo(4, "Cobán");               // E
+
+        // conexiones (ejemplo con distancias inventadas)
+        grafo.agregarArco(0, 1, 3); // A -> B
+        grafo.agregarArco(0, 3, 7); // A -> D
+        grafo.agregarArco(1, 2, 1); // B -> C
+        grafo.agregarArco(1, 4, 1); // B -> E
+        grafo.agregarArco(2, 3, 8); // C -> D
+        grafo.agregarArco(3, 4, 3); // D -> E
+        grafo.agregarArco(4, 1, 4); // E -> A
+        grafo.floydWarshall();
+        grafo.imprimirMatrizDistancias();
+
+        // mostrar ruta de A (0) a C (2)
+        System.out.print("Ruta más corta de Ciudad de Guatemala a Chiquimula: ");
+        grafo.imprimirRuta(0, 2);
+        System.out.println("Centro del grafo: " + grafo.obtenerCentroGrafo());
+    }
+}
