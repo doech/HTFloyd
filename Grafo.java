fc1a373c887f9052e4640b478d42b84ca3f019d7
María Sierra <sie24405@uvg.edu.gt>
2025-05-19 22:51:39 -0600

    Update Grafo.java

--- Grafo.java
+++ Grafo.java
@@ -1,1 +1,13 @@
+public class Grafo {
+    private final int;
+    private int[][] distancias;
+    private int[][] camino;
+    private String[] nombresNodos;
+    private int numNodos;
 
+    public Grafo(int numNodos) {
+        this.numNodos = numNodos;
+        distancias = new int[numNodos][numNodos];
+        camino = new int[numNodos][numNodos];
+        nombresNodos = new String[numNodos];
+    }
